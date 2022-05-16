2cfcd9410e5392860b718d63a51985fd18bc2466
vanta blanta <vantablanta@gmail.com>
2022-05-16 15:38:50 +0300

    Heroku Deploy

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI  = os.getenv("DEV_SQLALCHEMY_DATABASE_URI").replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI  = os.getenv("DATABASE_URL").replace('postgres://', 'postgresql://')
 
 
 
