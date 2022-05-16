02104e586a3e6af236544ed7516077c54b1419c9
vanta blanta <vantablanta@gmail.com>
2022-05-16 15:14:52 +0300

    Heroku Deploy

--- app/requests.py
+++ app/requests.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 import requests
 load_dotenv()
 
-BASE_URL= os.getenv('BASE_URL')
-
+BASE_URL= 'http://quotes.stormconsultancy.co.uk/random.json'
 def get_quotes():
     response = requests.get(BASE_URL).json()
     return response
