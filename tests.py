69ad441aeff0ffa7ee5613417780bea9748d86d7
vanta blanta <vantablanta@gmail.com>
2022-05-16 16:08:47 +0300

    ubittest added

---
+++ tests.py
@@ -0,0 +1,38 @@
+import unittest
+from app.models import User,Blogs, Comments
+
+class UserModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User("Michelle", "michelle@gmail.com", "1234")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_user, User))
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.password is not None)
+    
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.secure_password
+
+class BlogModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_pitch = Blogs("Pitch Title","Pitch Body","User ID")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pitch, Blogs))
+
+
+class CommentModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_pitch = Comments("User ID","Comment Body","Pitch ID")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pitch, Comments))
+
+   
+if __name__ == '__main__':
+    unittest.main()
+
+
+        
